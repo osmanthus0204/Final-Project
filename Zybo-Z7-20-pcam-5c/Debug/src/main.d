fb63d3f565af9e07abe3f0b31d1f27b8100bfcae
osmanthus0204 <tlsdlr00a@gmail.com>
2025-08-02 23:29:41 +0900

    zybo z7_20 pcam5c vitis

---
+++ Zybo-Z7-20-pcam-5c/Debug/src/main.d
@@ -0,0 +1,120 @@
+src/main.o src/main.o: ../src/main.cc \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h \
+ ../src/platform/platform.h ../src/platform/platform_config.h \
+ ../src/ov5640/OV5640.h ../src/ov5640/I2C_Client.h \
+ ../src/ov5640/GPIO_Client.h ../src/ov5640/../hdmi/VideoOutput.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma_hw.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/bspconfig.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_assert.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma_i.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xdebug.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xvtc.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xvtc_hw.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xclk_wiz.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_cache.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xclk_wiz_hw.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xplatform_info.h \
+ ../src/ov5640/ScuGicInterruptController.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xscugic.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xscugic_hw.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_exception.h \
+ ../src/ov5640/PS_GPIO.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xgpiops.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xgpiops_hw.h \
+ ../src/ov5640/AXI_VDMA.h ../src/ov5640/PS_IIC.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xiicps.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xiicps_hw.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/MIPI_D_PHY_RX.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/MIPI_CSI_2_RX.h
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h:
+
+../src/platform/platform.h:
+
+../src/platform/platform_config.h:
+
+../src/ov5640/OV5640.h:
+
+../src/ov5640/I2C_Client.h:
+
+../src/ov5640/GPIO_Client.h:
+
+../src/ov5640/../hdmi/VideoOutput.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma_hw.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/bspconfig.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_assert.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma_i.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xdebug.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xvtc.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xvtc_hw.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xclk_wiz.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_cache.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xclk_wiz_hw.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xplatform_info.h:
+
+../src/ov5640/ScuGicInterruptController.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xscugic.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xscugic_hw.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_exception.h:
+
+../src/ov5640/PS_GPIO.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xgpiops.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xgpiops_hw.h:
+
+../src/ov5640/AXI_VDMA.h:
+
+../src/ov5640/PS_IIC.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xiicps.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xiicps_hw.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/MIPI_D_PHY_RX.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/MIPI_CSI_2_RX.h:
---
+++ Zybo-Z7-20-pcam-5c/Debug/src/ov5640/OV5640.d
@@ -0,0 +1,74 @@
+src/ov5640/OV5640.o src/ov5640/OV5640.o: ../src/ov5640/OV5640.cpp \
+ ../src/ov5640/OV5640.h ../src/ov5640/I2C_Client.h \
+ ../src/ov5640/GPIO_Client.h ../src/ov5640/../hdmi/VideoOutput.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma_hw.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/bspconfig.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_assert.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma_i.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xdebug.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xvtc.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xvtc_hw.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xclk_wiz.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_cache.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xclk_wiz_hw.h \
+ C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xplatform_info.h
+
+../src/ov5640/OV5640.h:
+
+../src/ov5640/I2C_Client.h:
+
+../src/ov5640/GPIO_Client.h:
+
+../src/ov5640/../hdmi/VideoOutput.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma_hw.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/bspconfig.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_assert.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xaxivdma_i.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xdebug.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xvtc.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xvtc_hw.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xclk_wiz.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_cache.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xclk_wiz_hw.h:
+
+C:/working/zybopcam_or/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xplatform_info.h:
